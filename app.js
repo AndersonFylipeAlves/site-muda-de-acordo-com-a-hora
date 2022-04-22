8666a44f35646bbd5e1ce2457287fcbd45ab6933
AndersonFylipeAlves <102261299+AndersonFylipeAlves@users.noreply.github.com>
2022-04-22 12:11:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function carregar(){
+let msg = document.getElementById('msg')
+let imagem = document.getElementById('imagem')
+let data = new Date()
+let hora = hora.getHours()
+// let hora  = 7
+msg.innerHTML = `agora são ${hora} horas`
+if (hora >= 0 && hora < 12){
+    imagem.src = 'img1.jpg'
+    document.body.style.background = '#f0bb9d'
+}else if (hora >= 12 && hora <= 18){
+    imagem.src = 'img2.jpg'
+    document.body.style.background = '#f87427'
+}else {
+    imagem.src = 'img3.jpg'
+    document.body.style.background = '#6d7a82'
+}
+}
